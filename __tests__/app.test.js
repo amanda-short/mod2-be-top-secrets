978395fc6384e30c717e05a3bdc53ab76a297d97
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-11-16 09:59:17 -0800

    test 1 create new user passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,38 +4,33 @@ const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
 
-const testUser = {
-  firstName: 'Test',
-  lastName: 'User',
-  email: 'test@example.com',
-  password: '321321',
-};
-
-const registerAndLogin = async (userProps = {}) => {
-  const password = userProps.password ?? testUser.password;
-
-  const agent = request.agent(app);
-
-  const user = await UserService.create({ ...testUser, ...userProps });
-
-  const { email } = user;
-  await agent.post('/api/v1/users/sessions').send({ email, password });
-  return [agent, user];
-};
-
-describe('top-secret routes', () => {
+describe('users', () => {
   beforeEach(() => {
     return setup(pool);
   });
-
   
-  it('logs in a user', () => {
-  
-  expect(1).toEqual(1);
-});
+  const testUser = {
+    firstName: 'Test',
+    lastName: 'User',
+    email: 'test@example.com',
+    password: '321321',
+  };
 
-afterAll(() => {
-  pool.end();
-});
+  it('POST /api/v1/users creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(testUser);
+    expect(res.status).toBe(200);
+    const { firstName, lastName, email } = testUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
 });
 
