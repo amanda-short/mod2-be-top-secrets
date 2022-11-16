daa072c8c9302e48aa00d25c877daa06d22af715
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-11-16 09:22:31 -0800

    reset commit

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,41 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
-describe('backend-express-template routes', () => {
+const testUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '321321',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? testUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...testUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
+describe('top-secret routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
-  afterAll(() => {
-    pool.end();
-  });
+
+  
+  it('logs in a user', () => {
+  
+  expect(1).toEqual(1);
 });
+
+afterAll(() => {
+  pool.end();
+});
+});
+
